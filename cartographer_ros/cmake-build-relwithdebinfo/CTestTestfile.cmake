3ed5ca12020754935bcf3c7874e2c844f00c6b0a
erick lee <tensorflowcoralmovidius@gmail.com>
2022-08-23 13:32:47 +0900

    Deleted clion files

--- cartographer_ros/cmake-build-relwithdebinfo/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/erick/dependency_ws/src/cartographer_ros/cartographer_ros
-# Build directory: /home/erick/dependency_ws/src/cartographer_ros/cartographer_ros/cmake-build-relwithdebinfo
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
